4ef2c6f7b6b24f6d25896945a4928a26b29bd4b7
willowdev <patryk.wierzba.pw@gmail.com>
2021-03-29 22:02:50 +0200

    Lab2 Zad2

--- ksiazki.szczegoly.php
+++ ksiazki.szczegoly.php
@@ -15,13 +15,58 @@ use Ibd\Ksiazki;
 $ksiazki = new Ksiazki();
 $dane = $ksiazki->pobierz($id);
 ?>
+    <p>
+        <a href="ksiazki.lista.php"><i class="fas fa-chevron-left"></i> Powrót</a>
+    </p>
+    <div class="row">
+        <div class="col-4 p-1">
+            <?php if (!empty($dane['zdjecie'])) : ?>
+                <img src="zdjecia/<?= $dane['zdjecie'] ?>" alt="<?= $dane['tytul'] ?>" class="img-thumbnail" />
+            <?php else : ?>
+                brak zdjęcia
+            <?php endif; ?>
+        </div>
+        <div class="col-8">
+            <div class="table">
+                <table>
+                    <tr>
+                        <td>
+                            <h3> Tytuł: </h3>
+                        </td>
+                        <td>
+                           <h3> <?= $dane['tytul'] ?> </h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Cena: </td>
+                        <td>
+                            <?= $dane['cena'] ?> zł
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Liczba stron: </td>
+                        <td>
+                            <?= $dane['liczba_stron'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ISBN: </td>
+                        <td>
+                            <?= $dane['isbn'] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Opis: </td>
+                        <td  class="text-justify">
+                            <?= $dane['opis'] ?>
+                        </td>
+                    </tr>
 
-<h2><?=$dane['tytul']?></h2>
+                </table>
+            </div>
+        </div>
+    </div>
 
-<p>
-	<a href="ksiazki.lista.php"><i class="fas fa-chevron-left"></i> Powrót</a>
-</p>
 
-<p>szczegóły książki......</p>
 
 <?php include 'footer.php'; ?>
