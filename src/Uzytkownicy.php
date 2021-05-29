c8c9faf20d2c02fe9aa98d745bb51c19414989df
willowdev <patryk.wierzba.pw@gmail.com>
2021-05-29 22:35:17 +0200

    Zajecia 5 - Zadanie 4 (bcrypt)

--- src/Uzytkownicy.php
+++ src/Uzytkownicy.php
@@ -32,7 +32,7 @@ class Uzytkownicy
             'telefon' => $dane['telefon'],
             'email' => $dane['email'],
             'login' => $dane['login'],
-            'haslo' => md5($dane['haslo']),
+            'haslo' => password_hash(($dane['haslo']), PASSWORD_BCRYPT),
             'grupa' => $grupa
         ]);
     }
@@ -47,12 +47,13 @@ class Uzytkownicy
      */
     public function zaloguj(string $login, string $haslo, string $grupa): bool
     {
-        $haslo = md5($haslo);
+
         $dane = $this->db->pobierzWszystko(
-            "SELECT * FROM uzytkownicy WHERE login = :login AND haslo = '$haslo' AND grupa = '$grupa'", ['login' => $login]
+            "SELECT uz.haslo, uz.login, uz.grupa, uz.id FROM uzytkownicy uz WHERE login = :login AND grupa = '$grupa'", ['login' => $login]
         );
 
-        if ($dane) {
+
+        if (password_verify($haslo, $dane[0]['haslo'])) {
             $_SESSION['id_uzytkownika'] = $dane[0]['id'];
             $_SESSION['grupa'] = $dane[0]['grupa'];
             $_SESSION['login'] = $dane[0]['login'];
